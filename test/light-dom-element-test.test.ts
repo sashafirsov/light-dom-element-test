fd030c3608e3128c25522011c6c9d72e8485833c
Sasha Firsov <suns@firsov.net>
2022-04-13 04:37:31 -0700

    linter fixes

--- test/light-dom-element-test.test.ts
+++ test/light-dom-element-test.test.ts
@@ -1,32 +1,43 @@
 import { html } from 'lit';
 import { fixture, expect } from '@open-wc/testing';
-import { LightDomElementTest } from '../src/LightDomElementTest.js';
+// eslint-disable-next-line import/extensions
+import { LightDomElementTest } from '../src';
 import '../src/light-dom-element-test.js';
 
 describe('LightDomElementTest', () => {
-  it('has a default title "Hey there" and counter 5', async () => {
-    const el = await fixture<LightDomElementTest>(html`<light-dom-element-test></light-dom-element-test>`);
+    it('has a default title "Hey there" and counter 5', async () => {
+        const el = await fixture<LightDomElementTest>(
+            html`<light-dom-element-test></light-dom-element-test>`
+        );
 
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
-  });
+        expect(el.title).to.equal('Hey there');
+        expect(el.counter).to.equal(5);
+    });
 
-  it('increases the counter on button click', async () => {
-    const el = await fixture<LightDomElementTest>(html`<light-dom-element-test></light-dom-element-test>`);
-    el.shadowRoot!.querySelector('button')!.click();
+    it('increases the counter on button click', async () => {
+        const el = await fixture<LightDomElementTest>(
+            html`<light-dom-element-test></light-dom-element-test>`
+        );
+        el.shadowRoot!.querySelector('button')!.click();
 
-    expect(el.counter).to.equal(6);
-  });
+        expect(el.counter).to.equal(6);
+    });
 
-  it('can override the title via attribute', async () => {
-    const el = await fixture<LightDomElementTest>(html`<light-dom-element-test title="attribute title"></light-dom-element-test>`);
+    it('can override the title via attribute', async () => {
+        const el = await fixture<LightDomElementTest>(
+            html`<light-dom-element-test
+                title="attribute title"
+            ></light-dom-element-test>`
+        );
 
-    expect(el.title).to.equal('attribute title');
-  });
+        expect(el.title).to.equal('attribute title');
+    });
 
-  it('passes the a11y audit', async () => {
-    const el = await fixture<LightDomElementTest>(html`<light-dom-element-test></light-dom-element-test>`);
+    it('passes the a11y audit', async () => {
+        const el = await fixture<LightDomElementTest>(
+            html`<light-dom-element-test></light-dom-element-test>`
+        );
 
-    await expect(el).shadowDom.to.be.accessible();
-  });
+        await expect(el).shadowDom.to.be.accessible();
+    });
 });
