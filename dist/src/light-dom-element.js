a89d969b4e5dc31a6fd6946ad1009d461d24550e
suns <suns@firsov.net>
2022-04-23 22:05:01 -0700

    update dependencies

--- dist/src/light-dom-element.js
+++ dist/src/light-dom-element.js
@@ -1,9 +1,2 @@
-import { CssChain as $ } from "css-chain";
-import ShadowDomElement from "shadow-dom-element/shadow-dom-element.js";
-export default class LightDomElement extends ShadowDomElement {
-    applyTemplate(t) { $(this).template(t); return this; }
-    attachShadow() { }
-    ;
-}
-window.customElements.define('light-dom-element', LightDomElement);
+import{CssChain as m}from"css-chain";import o from"shadow-dom-element/shadow-dom-element.js";export default class e extends o{applyTemplate(t){return m(this).template(t),this}attachShadow(){}}window.customElements.define("light-dom-element",e);
 //# sourceMappingURL=light-dom-element.js.map
